af03d65b8b2a73ebb01a8861e0c016e726b158d1
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-03-05 11:12:37 -0500

    onChange notes

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 const UserForm = () => {
+    const [username, setUsername] = useState("Gus");
+    const handleChange = (e) => {
+        setUsername(e.target.value)
+    }
     return (
         <form>
-            <input type="text" placeholder="username"/>
+            <input type="text" placeholder="username" value={username} onChange={handleChange}/>
             <button>Add me to list!</button>
         </form>
     )
